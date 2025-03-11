98a3a33f6a2b3a8fdcb0adb730276b77537fa9a2
Megha Pant <mp8774482@gmail.com>
2025-03-11 20:36:05 +0530

    hehe

--- condition.py
+++ condition.py
@@ -10,34 +10,40 @@ else:
     print("light is broken")"""
 
 
-# marks = int(input("marks:"))
-# if (marks >= 90 ):
-#     print("A")
-# elif (marks >= 80 and marks < 90):
-#     print("B")
-# elif (marks >= 70 and marks < 80):
-#     print("C")
-# else:
-#     print("D")
+"""marks = int(input("marks:"))
+if (marks >= 90):
+     print("A")
+elif (marks >= 80):
+    print("B")
+elif (marks >= 70):
+    print("C")
+elif (marks >= 60):
+    print("D")
+else:
+    print("fail")"""
 
 
 #  a=5 & g=m
 #  a =2 & g=f
-# a=int(input("a: "))
-# g=input("m/f:")
-# if((a==1 or a==2)and g=="m"):
-#     print("fee is 100")
-# elif(a==3 or a==4 or g=="f"):
-#     print("fee is 200")
-# elif(a==5 and g=="m"):
-#     print("fee is 300")
-# else:
-#     print("no fee")
 
-age = int(input("age:"))
+a=int(input("a: "))
+g=input("m/f:")
+if((a==1 or a==2)and g=="m"):
+     print("fee is 100")
+elif((a==1 or a==2)and g=="f"):
+        print("fee is 50")
+elif((a==3 or a==4)and g=="m"):
+    print("fee is 200")
+elif((a==3 or a==4)and g=="f"):
+    print("fee is 150")
+else:
+    print("fee is 250")
+
+
+"""age = int(input("age:"))
 name =("i am 19:")
 megha = "yess" if age == int(19) else "no"
-print(megha) 
+print(megha) """
 
 # name = input("My love is ")
 # if (name == "sahil bajaj"):
