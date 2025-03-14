0606122faa8b8bd4a021c6307310bafef959a1b5
Megha Pant <mp8774482@gmail.com>
2025-03-14 20:35:14 +0530

    hyy

--- condition.py
+++ condition.py
@@ -26,24 +26,20 @@ else:
 #  a=5 & g=m
 #  a =2 & g=f
 
-a=int(input("a: "))
-g=input("m/f:")
-if((a==1 or a==2)and g=="m"):
-     print("fee is 100")
-elif((a==1 or a==2)and g=="f"):
-        print("fee is 50")
-elif((a==3 or a==4)and g=="m"):
-    print("fee is 200")
-elif((a==3 or a==4)and g=="f"):
-    print("fee is 150")
-else:
-    print("fee is 250")
+# a=int(input("a: "))
+# g=input("m/f:")
+# if((a==1 or a==2)and g=="m"):
+#      print("fee is 100")
+# elif((a==1 or a==2)and g=="f"):
+#         print("fee is 50")
+# elif((a==3 or a==4)and g=="m"):
+#     print("fee is 200")
+# elif((a==3 or a==4)and g=="f"):
+#     print("fee is 150")
+# else:
+#     print("fee is 250")
 
 
-"""age = int(input("age:"))
-name =("i am 19:")
-megha = "yess" if age == int(19) else "no"
-print(megha) """
 
 # name = input("My love is ")
 # if (name == "sahil bajaj"):
@@ -52,3 +48,33 @@ print(megha) """
 #     print("yepp alwayssss sahils love")
 # else:
 #     print("naa chiii kabhi nahi")
+
+"""age = int(input("age:"))
+if (age >= 18):
+    print("eligible for vote")
+else:
+    print("not eligible for vote") """
+
+
+"""age= int(input("age:"))
+print("can vote") if age>=18 else print("cannot vote")"""
+# (code)             (condition) (else)     (code)
+
+
+
+"""age= int(input("age:"))
+can_vote="eligible for vote" if age>=18 else "not eligible for vote"
+print(can_vote)"""
+
+# age = int(input("age:")) 
+# vote =("yes","no") [age<18]
+# print(vote)  
+
+age = int(input("age:"))
+if (age < 18):
+    print("no")
+else:
+    print("yes")
+# sal=float(input("salary:"))
+# tax=sal*(0.1, 0.2) [sal>50000]
+# print(tax)
