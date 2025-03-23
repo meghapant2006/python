3e0b7fe892cf86f6004c8d273464c62c38349838
Megha Pant <mp8774482@gmail.com>
2025-03-23 20:26:17 +0530

    .

---
+++ chapter3/list.py
@@ -0,0 +1,36 @@
+# my_list = ["kaddu", 19, 45.5, "chirkut", 21, 47.5]
+# print(my_list)
+# print(type(my_list))
+# print(len(my_list))
+# print(my_list[0]m)
+# my_list([0][3])
+
+
+# my_list = ["kaddu", 19, 45.5, "chirkut", 21, 47.5]
+# my_list[0]="megha"
+# my_list[3]="sahil"
+# print(my_list[:]) 
+
+
+# list slicing........
+# marks=[65,87,54,84,34,65,45,67,56]
+# print(marks[1:9])
+# print(marks[:])
+# print(marks[0:6])
+# print(marks[::-1])
+
+
+# list methods...
+marks=[65,87,54,84,34,65,45,67,56]
+
+# marks.append(200)
+# marks.sort()
+# marks.sort(reverse=True)
+# marks.reverse()
+
+
+print(marks)
+
+
+
+ 
