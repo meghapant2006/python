4af3ad59aa7f4abb263b0df6c224ae6103053dc8
Megha Pant <mp8774482@gmail.com>
2025-04-03 21:34:26 +0530

    jgjh

--- chapter 4/dictionary.py
+++ chapter 4/dictionary.py
@@ -1,10 +1,32 @@
-info = {
-    "name": ["kaddu","karela"],
-    "learning": "python",
-    "age": [19, 21]
+# info = {
+#     "name": ["kaddu","karela"],
+#     "learning": "python",
+#     "age": [19, 21]
+# }
+
+# print(info["name"][1])
+# print(info["learning"])
+# print(info["age"])
+
+# print(type(info))
+
+# info["name"] = "mannusahilu"
+# info["surname"] = "pantbajaj"  # they both are use to update dict
+# print(info)
+
+# info.update({"sal": 12})
+# print(info)
+
+# null_dict ={}
+# null_dict["name"]="sahilmegha"
+# print(null_dict) 
+
+babu  kya hua dekh  to ho 
+
+# Nested dictionary
+student ={
+    "name":"megha",
+    "name":"sahil",
+    "subj" :["phy","chem","math"]
+
 }
-
-print(info["name"][1])
-print(info["learning"])
-print(info["age"])
-
