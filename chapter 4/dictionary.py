ce9ab3493a515ae3f104c17c0edef85625cf6989
Megha Pant <mp8774482@gmail.com>
2025-04-06 19:58:16 +0530

    ghhg

--- chapter 4/dictionary.py
+++ chapter 4/dictionary.py
@@ -19,14 +19,27 @@
 
 # null_dict ={}
 # null_dict["name"]="sahilmegha"
-# print(null_dict) 
+# print(null_dict)
 
-babu  kya hua dekh  to ho 
 
-# Nested dictionary
-student ={
-    "name":"megha",
-    "name":"sahil",
-    "subj" :["phy","chem","math"]
-
+# Nested dictionary:
+student = {
+    "name": "megha",
+    "namee": "sahil",
+    "subj": {
+        "phy" : 78,
+        "chem":66,
+        "math":99}
 }
+# print("marks in phy: " + str(student["subj"]["phy"]))
+# print(student["subj"]["chem"])
+# print(student["subj"]["math"])
+
+# method:
+print(len(student))
+print(list(student.keys()))
+print(list(student["subj"].keys()))
+print(list(student.values()))
+print(list(student["subj"].values()))
+print(student.items())
+hn wsp pe aao 
