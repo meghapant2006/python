3a6b9ed6bca1e3f1a59103d7b76af96b4d13a5fa
Megha Pant <mp8774482@gmail.com>
2025-04-06 22:08:17 +0530

    yeh

--- chapter 4/dictionary.py
+++ chapter 4/dictionary.py
@@ -30,18 +30,38 @@ student = {
         "phy" : 78,
         "chem":66,
         "math":99}
-}
+} 
 # print("marks in phy: " + str(student["subj"]["phy"]))
 # print(student["subj"]["chem"])
 # print(student["subj"]["math"])
 
 # method:
-print(len(student))
-print(list(student.keys()))
-print(list(student["subj"].keys()))
-print(list(student.values()))
-print(list(student["subj"].values()))
-print(student.items())
+# print(len(student))
+# print(list(student.keys()))
+# print(list(student["subj"].keys()))
+# print(list(student.values()))
+# print(list(student["subj"].values()))
+# print(list(student.items()))
+# # print(student("name"))     #error
+# print(student.get("namee2"))  #no error -> None
+
+# employee = {
+#     "name": "sahil",
+#     "age": 21,
+#     "salary": 700000000,
+#     "address": {
+#         "city": "delhi",  
+#         "state": "new delhi",
+#         "country": "india"
+#     }
+# }
+
+new_dict = {"name": "sahil", "age": 21, "salary": 700000000, "address": {"city": "delhi", "state": "new delhi", "country": "india"}}
+# print(student.update(employee))   # it will update the student dict with employee dict
+# print(student) 
+student.update({"name": "sahilis"})  # it will update the student dict with employee dict
+print(student)
+
 
 
  
---
+++ extra/graphpie.py
@@ -0,0 +1,34 @@
+# import matplotlib.pyplot as plt
+
+# # Data
+# labels = ['Apple', 'Banana', 'Cherry', 'Date']
+# sizes = [30, 25, 25, 20]
+# colors = ['red', 'yellow', 'pink', 'brown']
+
+# # Plot
+# plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+# plt.title('Fruit Distribution')
+# plt.axis('equal') 
+# explode = (0.1, 0, 0, 0)
+# plt.show()
+
+
+
+
+# import matplotlib.pyplot as plt
+
+# # Data
+# x = [1, 2, 3, 4, 5]
+# y = [10, 20, 15, 25, 30]
+
+# # Plot
+# plt.plot(x, y, marker='*', linestyle='-', color='blue',mfc='red')
+# plt.title('Simple Line Graph')
+# plt.xlabel('X Axis')
+# plt.ylabel('Y Axis')
+# plt.grid(True)
+# plt.show()
+
+ 
+
+
